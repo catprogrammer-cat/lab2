d6ddb4905a1cadbdd7a545a6b935740d9efacdc3
catprogrammer.cat <catprogrammer.vlad@gmail.com>
2019-09-14 20:44:37 +0300

    I done first table

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -1,20 +1,52 @@
-// lab2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+
+	/////
+	double x;
+	cout << "Enter varible x\n";
+	cin >> x;
+	system("cls");
+	if (x <= -8) {
+		cout << "You entered x = " << x << "\n";
+		cout << "*\n*\n*\n*\n";
+		cout << "     |6 * pow(x, 3) - 8 when (x <= -8) <-------- this condition is true\n"
+			<<	" Y = |pow(x, 3) - 8     when (-8 < x < 0)\n"
+			<<  "     |2 * pow(x, 2)     when (x >= 0)\n";
+		cout << "*\n*\n*\n*\n";
+		cout << "\tThan we have a expression:\n";
+		double Y = 6 * pow(x, 3) - 8;
+		cout << "Y = 6 * pow(x, 3) - 8\n";
+		cout << "Y = " << Y << ";\n";
+	}
+	else if (x > -8 && x < 0) {
+		cout << "You entered x = " << x << "\n";
+		cout << "*\n*\n*\n*\n";
+		cout << "     |6 * pow(x, 3) - 8 when (x <= -8)\n"
+			<< " Y = |pow(x, 3) - 8     when (-8 < x < 0) <-------- this condition is true\n"
+			<< "     |2 * pow(x, 2)     when (x >= 0)\n";
+		cout << "*\n*\n*\n*\n";
+		cout << "\tThan we have a expression:\n";
+		double Y = pow(x, 3) - 8;
+		cout << "Y = pow(x, 3) - 8\n";
+		cout << "Y = " << Y << ";\n";
+	}
+	else if (x >= 0) {
+		cout << "You entered x = " << x << "\n";
+		cout << "*\n*\n*\n*\n";
+		cout << "     |6 * pow(x, 3) - 8 when (x <= -8)\n"
+			<< " Y = |pow(x, 3) - 8     when (-8 < x < 0)\n"
+			<< "     |2 * pow(x, 2)     when (x >= 0) <-------- this condition is true\n";
+		cout << "*\n*\n*\n*\n";
+		cout << "\tThan we have a expression:\n";
+		double Y = 2 * pow(x, 2);
+		cout << "Y = 2 * pow(x, 2)\n";
+		cout << "Y = " << Y << ";\n";
+	}
+
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
